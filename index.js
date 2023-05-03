b37555752875009f8a352c0910afb6897b457d81
shafayetullah7 <shafayet769@gmail.com>
2023-05-03 12:53:55 +0600

    vercel.json

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+var cors = require('cors');
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World chef!')
-})
+});
+app.get('/chefs',(req,res)=>{
+  res.send(chefs);
+});
+app.get('/chefs/:id',(req,res)=>{
+  const id = req.params.id;
+  const chef = chefs.find(c => c.id===id);
+  res.send(chef);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
